4eb22ef1545a9e784dbe446f952136f869a62ad5
rachowell <rachel.howell@madetech.com>
2024-08-05 10:47:45 +0100

    EC-59 Move lambda and cloudfront resources into separate files
    Co-Authored-By: Korinna Pinakoulakis <7438240+kpinakula@users.noreply.github.com>

---
+++ ecaas-infrastructure/modules/api_gateway/cloudfront.tf
@@ -0,0 +1,40 @@
+resource "aws_cloudfront_distribution" "api_gateway_cloudfront_distribution" {
+  enabled = true
+
+  origin {
+    domain_name = "${aws_api_gateway_rest_api.ECaaSAPI.id}.execute-api.${var.region}.amazonaws.com"
+    origin_id   = "ApiGatewayOrigin"
+    custom_origin_config {
+      http_port              = "80"
+      https_port             = "443"
+      origin_protocol_policy = "https-only"
+      origin_ssl_protocols   = ["TLSv1.2"]
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  default_cache_behavior {
+    allowed_methods  = ["HEAD", "DELETE", "POST", "GET", "OPTIONS", "PUT", "PATCH"]
+    cached_methods   = ["HEAD", "GET", "OPTIONS"]
+    target_origin_id = "ApiGatewayOrigin"
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "allow-all"
+  }
+}
---
+++ ecaas-infrastructure/modules/api_gateway/lambda.tf
@@ -0,0 +1,68 @@
+data "archive_file" "aws_lambda_placeholder_archive" {
+  type        = "zip"
+  source_file = "bootstrap"
+  output_path = "bootstrap.zip"
+}
+
+resource "aws_lambda_function" "hem_lambda" {
+  filename      = data.archive_file.aws_lambda_placeholder_archive.output_path
+  function_name = "hem-lambda"
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "bootstrap"
+  runtime       = "provided.al2023"
+  architectures = ["arm64"]
+  timeout       = 60
+  memory_size   = 3072
+}
+
+resource "aws_iam_role" "lambda_role" {
+  name               = "lambda-role"
+  assume_role_policy = data.aws_iam_policy_document.assume_role_lambda.json
+}
+
+data "aws_iam_policy_document" "assume_role_lambda" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+data "aws_iam_policy_document" "lambda_logging" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+    ]
+
+    resources = ["arn:aws:logs:*:*:*"]
+  }
+}
+
+resource "aws_iam_policy" "lambda_logging" {
+  name        = "lambda_logging"
+  path        = "/"
+  description = "IAM policy for logging from a lambda"
+  policy      = data.aws_iam_policy_document.lambda_logging.json
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_logs" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = aws_iam_policy.lambda_logging.arn
+}
+
+resource "aws_lambda_permission" "api_gateway_lambda_permission" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.hem_lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.ECaaSAPI.execution_arn}/*/*"
+}
