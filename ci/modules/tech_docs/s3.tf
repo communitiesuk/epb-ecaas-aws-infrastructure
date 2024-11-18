4b2997a55f23776d7a20a99f516196a8c19d0406
rachowell <rachel.howell@madetech.com>
2024-11-18 13:08:48 +0000

    Amend tech docs s3 public access policy
    The block_public_policy is set to true in the console but was false in the code.

--- ci/modules/tech_docs/s3.tf
+++ ci/modules/tech_docs/s3.tf
@@ -6,7 +6,7 @@ resource "aws_s3_bucket" "tech-docs-s3" {
 resource "aws_s3_bucket_public_access_block" "block_public_access" {
   bucket                  = aws_s3_bucket.tech-docs-s3.id
   block_public_acls       = true
-  block_public_policy     = false
+  block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
