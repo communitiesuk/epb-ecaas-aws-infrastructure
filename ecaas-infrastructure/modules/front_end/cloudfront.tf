5f8605eefbfba3454d422b557ba3acc6826a9e2a
James Wainwright <james.wainwright@madetech.com>
2025-05-29 16:15:37 +0100

    EC-503 - Allow all cookies in frontend cache policy

--- ecaas-infrastructure/modules/front_end/cloudfront.tf
+++ ecaas-infrastructure/modules/front_end/cloudfront.tf
@@ -84,7 +84,7 @@ resource "aws_cloudfront_cache_policy" "this" {
   max_ttl     = 86400
   parameters_in_cache_key_and_forwarded_to_origin {
     cookies_config {
-      cookie_behavior = "none"
+      cookie_behavior = "all"
     }
     headers_config {
       header_behavior = "whitelist"
