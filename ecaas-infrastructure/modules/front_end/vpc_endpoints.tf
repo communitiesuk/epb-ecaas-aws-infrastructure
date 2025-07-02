eebe7b8c4ca9cbad01b2fb5e1ea78fd4dcfc2a83
Harriet Hall <harriet.hall@madetech.com>
2025-07-02 10:16:41 +0100

    EC-654 - add subnet_ids to VPC Endpoint resources

--- ecaas-infrastructure/modules/front_end/vpc_endpoints.tf
+++ ecaas-infrastructure/modules/front_end/vpc_endpoints.tf
@@ -1,21 +1,17 @@
 resource "aws_vpc_endpoint" "api_gateway_vpc_access" {
   vpc_id       = aws_vpc.this.id
   service_name = "com.amazonaws.${var.region}.apigateway"
-    vpc_endpoint_type = "Interface"
-  security_group_ids = [
-    aws_security_group.api-gateway_endpoint_sg.id,
-  ]
-
+  vpc_endpoint_type = "Interface"
+  security_group_ids = [aws_security_group.api-gateway_endpoint_sg.id]
+  subnet_ids = aws_subnet.private.*.id
   private_dns_enabled = true
 }
 
 resource "aws_vpc_endpoint" "ssm_vpc_access" {
   vpc_id       = aws_vpc.this.id
   service_name = "com.amazonaws.${var.region}.ssm"
-    vpc_endpoint_type = "Interface"
-  security_group_ids = [
-    aws_security_group.ssm_endpoint_sg.id,
-  ]
-
+  vpc_endpoint_type = "Interface"
+  security_group_ids = [aws_security_group.ssm_endpoint_sg.id]
+  subnet_ids = aws_subnet.private.*.id
   private_dns_enabled = true
 }
