8b054218af16baec902490d0ed24c0f6fb222d27
Kev Keenoy <kevin.keenoy@communities.gov.uk>
2025-07-21 17:49:37 +0100

    Remove now-unused frontend SSL certificate
    This has been replaced by the frontend_cdn_certificate module that uses the "ssl" module.

--- ecaas-infrastructure/modules/front_end/certificate.tf
+++
@@ -1,9 +0,0 @@
-# TODO: DELETE THIS AND ADJUST REFERENCES TO IT ONCE THE NEW CERT IS IN PLACE
-resource "aws_acm_certificate" "cert-cdn" {
-  domain_name       = var.domain_name
-  validation_method = "DNS"
-  lifecycle {
-    create_before_destroy = true
-  }
-  provider = aws.us-east
-}
