317e7936c9e8ae81706b428947e5be67392280de
Harriet Hall <harriet.hall@madetech.com>
2025-07-23 12:05:55 +0100

    EC-613: update the base docker image as per the playwright docs

--- ci/modules/front-end-pipeline/modules.tf
+++ ci/modules/front-end-pipeline/modules.tf
@@ -50,7 +50,7 @@ module "codebuild_e2e_test_front_end" {
   name                       = "${var.project_name}-codebuild-e2e_test-front-end"
   codebuild_compute_type     = "BUILD_GENERAL1_MEDIUM"
   codebuild_environment_type = "LINUX_CONTAINER"
-  build_image_uri            = "aws/codebuild/amazonlinux-x86_64-standard:5.0"
+  build_image_uri            = "mcr.microsoft.com/playwright:v1.54.0-noble"
   buildspec_file             = "buildspec/e2e_test_front_end.yml"
   region                     = var.region
   environment_variables      = [
