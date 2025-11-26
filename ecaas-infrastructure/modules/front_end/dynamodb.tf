161ae137304cf9001e4f8115a53ce041574b5b55
James Wainwright <james.wainwright@madetech.com>
2025-11-26 10:07:59 +0000

    EC-1073 - DynamoDB table for PCDB products

--- ecaas-infrastructure/modules/front_end/dynamodb.tf
+++ ecaas-infrastructure/modules/front_end/dynamodb.tf
@@ -16,4 +16,57 @@ resource "aws_dynamodb_table" "user_sessions_table" {
   }
 }
 
+resource "aws_dynamodb_table" "products_table" {
+  name = "products"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key = "id"
 
+  attribute {
+	name = "id"
+	type = "N"
+  }
+
+  attribute {
+	name = "technologyType"
+	type = "S"
+  }
+
+  attribute {
+	name = "modelQualifier"
+	type = "S"
+  }
+
+  attribute {
+	name = "sk-by-brand"
+	type = "S"
+  }
+
+  attribute {
+	name = "sk-by-model"
+	type = "S"
+  }
+
+  global_secondary_index {
+	name = "by-brand"
+	hash_key = "technologyType"
+	range_key = "sk-by-brand"
+	projection_type = "INCLUDE"
+	non_key_attributes = ["id", "brandName", "modelName", "modelQualifier"]
+  }
+
+  global_secondary_index {
+	name = "by-model"
+	hash_key = "technologyType"
+	range_key = "sk-by-model"
+	projection_type = "INCLUDE"
+	non_key_attributes = ["id", "brandName", "modelName", "modelQualifier"]
+  }
+
+  global_secondary_index {
+	name = "by-model-qualifier"
+	hash_key = "technologyType"
+	range_key = "modelQualifier"
+	projection_type = "INCLUDE"
+	non_key_attributes = ["id", "brandName", "modelName", "modelQualifier"]
+  }
+}
