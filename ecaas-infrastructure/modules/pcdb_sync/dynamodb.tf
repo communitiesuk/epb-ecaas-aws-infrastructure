193421f1a50af1414250c4cfe5ef3e8c83eeab4c
James Wainwright <james.wainwright@madetech.com>
2026-02-25 13:23:34 +0000

    EC-1223: Add communalHeatNetworkName to by-brand index

--- ecaas-infrastructure/modules/pcdb_sync/dynamodb.tf
+++ ecaas-infrastructure/modules/pcdb_sync/dynamodb.tf
@@ -34,7 +34,7 @@ resource "aws_dynamodb_table" "products_table" {
 	hash_key = "technologyType"
 	range_key = "sk-by-brand"
 	projection_type = "INCLUDE"
-	non_key_attributes = ["id", "brandName", "modelName", "modelQualifier", "boilerLocation"]
+	non_key_attributes = ["id", "brandName", "modelName", "modelQualifier", "boilerLocation", "communityHeatNetworkName"]
   }
 
   global_secondary_index {
