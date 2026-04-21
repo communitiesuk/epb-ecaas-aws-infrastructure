b52b808ad6b6794d855d80640d96a57f98974b54
kpinakula <kpinakula@users.noreply.github.com>
2026-04-21 18:17:08 +0100

    Remove fhs_wrapper from pipeline input_artifacts
    This commit removes the fhs_wrapper_output_source from the
    input_artifacts of the ecaas-api pipeline as this was not needed.
    
    Co-Authored-By: Rachel Howell <98879818+rachowell@users.noreply.github.com>

--- ci/modules/ecaas_api_pipeline/code_pipeline.tf
+++ ci/modules/ecaas_api_pipeline/code_pipeline.tf
@@ -103,7 +103,7 @@ resource "aws_codepipeline" "codepipeline" {
       owner            = "AWS"
       provider         = "CodeBuild"
       version          = "1"
-      input_artifacts  = ["source_output", "hem_core_source_output", "fhs_wrapper_source_output"]
+      input_artifacts  = ["source_output", "hem_core_source_output"]
       output_artifacts = ["build_hem_lambda_output"]
 
       configuration = {
